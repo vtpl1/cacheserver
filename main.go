e0271c790af50c67bebf1aaf2aeced447d9207a2
Monotosh Das <monotosh.das@videonetics.com>
2024-12-30 12:20:40 +0000

    wip

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ import (
 )
 
 func getFolder(s string) string {
-	err := os.MkdirAll(s, os.ModeDir)
+	err := os.MkdirAll(s, 0o750)
 	if err != nil {
 		fmt.Printf("Unable to create folder %s, %v", s, err)
 	}
